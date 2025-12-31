3fb33c5d208a43842ec358c6352b88b18b1f958d
Oluwaseun Adanike <oluwaseun.adanike@ng.kpmg.com>
2025-12-31 10:03:01 +0100

    SQL for subscription to organisation management

--- src/main/resources/db/migration/V1_9__Rename_GymSubscriptions_To_Subscriptions.sql
+++ src/main/resources/db/migration/V1_9__Rename_GymSubscriptions_To_Subscriptions.sql
@@ -1,10 +1,10 @@
--- Rename gym_subscriptions table to organisation_subscriptions
+-- Rename gym_subscriptions table to subscriptions
 ALTER TABLE gym_subscriptions
-  RENAME TO organisation_subscriptions;
+  RENAME TO subscriptions;
 
--- Rename gym_id column to organisation_id in organisation_subscriptions
+-- Rename gym_id column to organisation_id in subscriptions
 -- (Already updated in code, but need to ensure constraint names are correct)
-ALTER TABLE organisation_subscriptions
+ALTER TABLE subscriptions
   RENAME COLUMN gym_id TO organisation_id;
 
 -- Add unique constraint on organisation_id if not exists
@@ -12,10 +12,10 @@ DO $$
 BEGIN
     IF NOT EXISTS (
         SELECT 1 FROM pg_constraint
-        WHERE conname = 'uk_organisation_subscriptions_organisation_id'
+        WHERE conname = 'uk_subscriptions_organisation_id'
     ) THEN
-        ALTER TABLE organisation_subscriptions
-          ADD CONSTRAINT uk_organisation_subscriptions_organisation_id UNIQUE (organisation_id);
+        ALTER TABLE subscriptions
+          ADD CONSTRAINT uk_subscriptions_organisation_id UNIQUE (organisation_id);
     END IF;
 END $$;
 
@@ -24,7 +24,7 @@ ALTER TABLE api_rate_limits
   RENAME COLUMN gym_id TO organisation_id;
 
 -- Add comment to table
-COMMENT ON TABLE organisation_subscriptions IS 'Subscription information for organisations (one subscription per organisation covers all gyms)';
-COMMENT ON COLUMN organisation_subscriptions.organisation_id IS 'Reference to the organisation that owns this subscription';
+COMMENT ON TABLE subscriptions IS 'Subscription information for organisations (one subscription per organisation covers all gyms)';
+COMMENT ON COLUMN subscriptions.organisation_id IS 'Reference to the organisation that owns this subscription';
 COMMENT ON COLUMN api_rate_limits.organisation_id IS 'Reference to the organisation being rate limited';
 
